7eea081a5cc36604baddd31be5f705b2ff1e1f08
noamkumar <kumarnoam00@gmail.com>
2026-09-22 16:56:52 +0530

    Allow Vercel frontend in CORS

--- scholarship-management-backend/main.py
+++ scholarship-management-backend/main.py
@@ -43,13 +43,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://scholarship-management-system.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
-
 
 # ==========================================
 # CREATE DATABASE TABLES
